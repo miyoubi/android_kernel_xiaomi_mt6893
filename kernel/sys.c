77d7d76370c7e40667ef199fda36d96ed5800969
exer <dayesofficial@gmail.com>
2025-07-28 14:21:10 +0000

    syscall: Spoof BPF to 4.19.280

--- kernel/sys.c
+++ kernel/sys.c
@@ -1193,12 +1193,12 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-	if (!strncmp(current->comm, "netbpfload", 10) &&
-	    current->pid != netbpfload_pid) {
-		netbpfload_pid = current->pid;
-		strcpy(tmp.release, "6.6.40");
-		pr_debug("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
+	if (!strncmp(current->comm, "bpfloader", 9) ||
+	    !strncmp(current->comm, "netbpfload", 10) ||
+	    !strncmp(current->comm, "netd", 4)) {
+		strcpy(tmp.release, "4.19.280");
+		pr_debug("uname: %s release=%s\n",
+			 current->comm, tmp.release);
 	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
